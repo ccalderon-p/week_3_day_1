614e5f4ed00edcc2a19bc4fb1e009db77a31d5fd
Calderon-Parra <ccalderon-p@cps.k12.il.us>
2024-09-19 12:12:10 -0500

    Valid Variable

---
+++ week_3/testColab.py
@@ -0,0 +1,7 @@
+#this is just for testing for collab purposes
+#create an input for the user to input their age
+#create an input for the user to input their name 
+#create an input for the user to input their favorite song
+
+
+
